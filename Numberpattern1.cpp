6a50d0ee2c6821a694ea231d5b967903c2e14b25
Tusshar0910 <107608009+Tusshar0910@users.noreply.github.com>
2022-08-29 11:34:23 +0530

    Create Numberpattern1.cpp

---
+++ Numberpattern1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i =1; 1<=n; i++) {
+        int t = 1;
+        for (int j = i; t<= i; t++, j++) {
+            cout << j;
+        }
+        cout << endl;
+   }
+   return 0;
+}
+   
+  
+    
